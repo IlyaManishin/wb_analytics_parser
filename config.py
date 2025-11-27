a39ba99f9f2b44d2079af70f3988da4f7d3a2912
IlyaManishin <manishin06@gmail.com>
2025-11-27 22:14:58 +0700

    change log format

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format='[%(asctime)s] %(levelname)s %(name)s: %(message)s',
+    format=r"%(asctime)s - [%(levelname)s] -  %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s",
     datefmt='%Y-%m-%d %H:%M:%S',
     handlers=[
         logging.FileHandler("logger.log"),
